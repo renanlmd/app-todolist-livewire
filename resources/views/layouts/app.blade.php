da4244b6a8a837f26305e99503896be35f2c49fc
Renan Almeida <renan.webtec@gmail.com>
2021-05-24 14:52:12 -0300

    deploy assets css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>App Todo List</title>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="/css/app.css" rel="stylesheet">
     @livewireStyles
 
 </head>
